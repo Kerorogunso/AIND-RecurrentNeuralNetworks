58ed81e43c6bec03c29bd01b19177ac7490b457a
Albert <albertswchung@gmail.com>
2018-03-06 21:17:13 +0000

    Initial Upload

--- my_answers.py
+++ my_answers.py
@@ -10,8 +10,8 @@ import keras
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
-    X = []
-    y = []
+    X = [series[i:i+window_size] for i in range(0, len(series)-window_size)]
+    y = [series[window_size+i] for i in range(0, len(series)-window_size)]
 
     # reshape each 
     X = np.asarray(X)
@@ -23,13 +23,22 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size,1)))
+    model.add(Dense(1))
+    
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
+    valid = set(list('abcdefghijklmnopqrstuvwxyz')).union(set(punctuation))
+    characters = set(list(text)).difference(valid)
+    
+    for c in characters:
+        text = text.replace(c, ' ')
+        
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -37,10 +46,17 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    
+    inputs = [text[i:i+window_size] for i in range(0,len(text)-window_size, step_size)]
+    outputs = [text[i+window_size] for i in range(0, len(text)-window_size, step_size)]
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars,activation='sigmoid'))
+    
+    return model
